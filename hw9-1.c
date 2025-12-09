ad73123395a854f43cc72447304c3a8ab4d67f5b
z125518-Lucas-Kato <z125518@shibaura-it.ac.jp>
2025-12-09 13:30:55 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,31 @@
+// Create a program that read in 2 x 2 matrix and
+// then calculate the determinant of that matrix.
 
+#include <stdio.h>
+
+int main() {
+    int a[2][2];
+    int i, j;
+    double det = 0;
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    printf("You entered\n");
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+    
+    printf("Determinant is %f", det);
+
+    return 0;
+}

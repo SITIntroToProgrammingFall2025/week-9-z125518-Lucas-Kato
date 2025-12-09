b72a2b86fa94ac5a075d546b425e71ad1aaa65b6
z125518-Lucas-Kato <z125518@shibaura-it.ac.jp>
2025-12-09 14:54:41 +0900

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,43 @@
+// Create a program that would calculate
+// any input RGB values to hexadecimal
+// code
+
+#include <stdio.h>
+
+int main() {
+    
+    char hexadecimal[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    
+    double rgb[1][3]; 
+    int j;
+    double num = 0;
+    char hex[20] = "#";
+    int count = 1;
+
+
+    for (j = 0; j < 3; j++) {
+        scanf("%lf", &rgb[0][j]);
+    }
+
+    for (j = 0; j < 3; j++) {
+        num = rgb[0][j];
+        num = num / 16;
+        int inteira = (int)num;
+        hex[count] = hexadecimal[inteira];
+        count++;
+        double decimal = num - inteira;
+        int decimal2 = (int)(decimal * 16);
+        hex[count] = hexadecimal[decimal2];
+        count++;
+    }
+    
+    for (int i = 0; i < count; i++){
+        printf("%c", hex[i]);
+    }
+    
+    
+
+    return 0;
+}
+
 
